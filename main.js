f755ce8f5c073052530eb7ef4c62c3eeb71ea2e3
Diễn Nguyễn <nhdien07122012@gmail.com>
2024-12-11 15:58:15 +0700

    cập nhật giao diện

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.getElementById("menu-toggle");
     const sidebar = document.getElementById("sidebar");
     const mainContent = document.getElementById("main-content");
+    const qrToggle = document.getElementById("qr-toggle");
+    const qrCode = document.getElementById("qr-code");
 
     menuToggle.addEventListener("click", function() {
         sidebar.classList.toggle("active");
         mainContent.classList.toggle("active");
+    });
+
+    qrToggle.addEventListener("click", function() {
+        qrCode.style.display = qrCode.style.display === "flex" ? "none" : "flex";
     });
 });
